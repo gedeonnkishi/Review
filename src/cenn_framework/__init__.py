b3a169a548adc3f4b6d61aa3c1993ba7ca1bd85f
nkishiged <nkishiged@gmail.com>
2026-01-22 15:10:52 +0100

    Add docstring and version to __init__.py
    Added module docstring and versioning information.

--- src/cenn_framework/__init__.py
+++ src/cenn_framework/__init__.py
@@ -1,1 +1,9 @@
+"""
+CeNN Framework for Quantum-Inspired Time Series Forecasting
+"""
 
+from .cenn_cell import CeNNCell
+from .cenn_emulator import CeNNEmulator
+
+__version__ = "1.0.0"
+__all__ = ['CeNNCell', 'CeNNEmulator']
